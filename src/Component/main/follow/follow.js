6d945bc9a2f91c829650b398e0cccb9596769d06
DESKTOP-Q8PHHQK\Leesangheon <yahoo9886@kakao.com>
2021-02-04 09:57:22 +0900

    2021 02 04

---
+++ src/Component/main/follow/follow.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class follow extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default follow;
