c771cb2b61bc49f5da0f7d26662ae276b686cde5
DESKTOP-BJRN072\ysy89 <ysy8932@gmail.com>
2021-02-05 10:53:37 +0900

    header, main

--- src/Component/header/header.js
+++ src/Component/header/header.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { NavLink } from 'react-router-dom';
+import { BiSearchAlt } from "react-icons/bi";
+import { FaWaze } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { FaLaughWink } from "react-icons/fa";
 
-const header = () => {
-    const activeStyle = {
+
+class header extends Component {    
+    render() {
+        const activeStyle = {
         color: 'green',
         fontSize: '2rem'
+        };
+
+        return (
+            <div>
+               
+                        <NavLink exact to="/main" 
+                    activeStyle={activeStyle}>오늘의회식</NavLink>
+                    
+                        <NavLink exact to="/main/search" 
+                        activeStyle={activeStyle}><BiSearchAlt />search</NavLink>
+                   
+                        <NavLink exact to="/main/chatting" 
+                        activeStyle={activeStyle}><FaWaze/>chat</NavLink>
+                    
+                        <NavLink exact to="/main/follow" 
+                        activeStyle={activeStyle}><FaHeart />follow</NavLink>
+                    
+                        <NavLink exact to="/main/mypage" 
+                        activeStyle={activeStyle}>< FaLaughWink/>mypage</NavLink>
+                   
+                <hr/>
+            </div>
+        );
     };
-
-    return (
-        <div>
-            <ul>
-                <li><NavLink exact to="/main" activeStyle={activeStyle}>main</NavLink></li>
-                <li><NavLink exact to="/main/mypage" activeStyle={activeStyle}>mypage</NavLink></li>
-    
-            </ul>
-            <hr/>
-        </div>
-    );
-};
-
+}
 export default header;
+
+ /* static navigationOptions = {
+    headerLeft: <NavLink exact to="/main/chatting" activeStyle={activeStyle}>chat</NavLink>,
+    title: <NavLink exact to="/main" activeStyle={activeStyle}>오늘의회식</NavLink>
+    headerRight: <NavLink exact to="/main/search" activeStyle={activeStyle}>search</NavLink>,
+                <NavLink exact to="/main/follow activeStyle={activeStyle}>follow</NavLink>,
+                <NavLink exact to="/main/mypage" activeStyle={activeStyle}>mypage</NavLink>
+    }*/
